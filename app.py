c2975cd019fec47efb07a19114a14b5a007a56bc
faust8888 <faust.ilya@gmail.com>
2019-01-28 23:40:41 +0300

    python 2.7

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
 import time
+import BaseHTTPServer
 import os
-from http.server import BaseHTTPRequestHandler, HTTPServer
 
 
 HOST_NAME = 'localhost'
-PORT_NUMBER = 8080
+PORT_NUMBER = 8007
 
 
-class MyHandler(BaseHTTPRequestHandler):
+class MyHandler(BaseHTTPServer.BaseHTTPRequestHandler):
     def do_HEAD(s):
         s.send_response(200)
         s.send_header("Content-type", "text/html")
@@ -18,16 +18,17 @@ class MyHandler(BaseHTTPRequestHandler):
         s.end_headers()
         s.wfile.write("<html><head><title>Title goes here.</title></head>")
         s.wfile.write("<body><p>This is a test.</p>")
-        s.wfile.write("<time.asctime()>HOME: %s</p>" % os.environ['HOME'])
+        s.wfile.write("<p>HOME: %s</p>" % os.environ['HOME'])
         s.wfile.write("<p>ILYA_MERKUREV_VAR: %s</p>" % os.environ['ILYA_MERKUREV_VAR'])
         s.wfile.write("</body></html>")
 
 if __name__ == '__main__':
-    server_class = HTTPServer
+    server_class = BaseHTTPServer.HTTPServer
     httpd = server_class((HOST_NAME, PORT_NUMBER), MyHandler)
-    print (time.asctime(), "Server Starts - %s:%s" % (HOST_NAME, PORT_NUMBER))
+    print time.asctime(), "Server Starts - %s:%s" % (HOST_NAME, PORT_NUMBER)
     try:
         httpd.serve_forever()
     except KeyboardInterrupt:
         pass
     httpd.server_close()
+    print time.asctime(), "Server Stops - %s:%s" % (HOST_NAME, PORT_NUMBER)
