fc6220802aa9ac61eba726dafd43f4fe09bc5ab2
faust8888 <faust.ilya@gmail.com>
2019-01-28 22:18:19 +0300

    change syntaxis

--- app.py
+++ app.py
@@ -18,17 +18,16 @@ class MyHandler(BaseHTTPServer.BaseHTTPRequestHandler):
         s.end_headers()
         s.wfile.write("<html><head><title>Title goes here.</title></head>")
         s.wfile.write("<body><p>This is a test.</p>")
-        s.wfile.write("<p>HOME: %s</p>" % os.environ['HOME'])
+        s.wfile.write("<time.asctime()>HOME: %s</p>" % os.environ['HOME'])
         s.wfile.write("<p>ILYA_MERKUREV_VAR: %s</p>" % os.environ['ILYA_MERKUREV_VAR'])
         s.wfile.write("</body></html>")
 
 if __name__ == '__main__':
     server_class = BaseHTTPServer.HTTPServer
     httpd = server_class((HOST_NAME, PORT_NUMBER), MyHandler)
-    print time.asctime(), "Server Starts - %s:%s" % (HOST_NAME, PORT_NUMBER)
+    print (time.asctime(), "Server Starts - %s:%s" % (HOST_NAME, PORT_NUMBER))
     try:
         httpd.serve_forever()
     except KeyboardInterrupt:
         pass
     httpd.server_close()
-    print time.asctime(), "Server Stops - %s:%s" % (HOST_NAME, PORT_NUMBER)
