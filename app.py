40d726a5411c4ff84a3525d635d6e432315d9012
faust8888 <faust.ilya@gmail.com>
2019-02-28 17:36:30 +0300

    logging into Elasticsearch directly

--- app.py
+++ app.py
@@ -3,10 +3,16 @@ import time
 import os
 import logging
 from http.server import BaseHTTPRequestHandler, HTTPServer
+import cloudDataLogging
 
+
+MEF_APPLICATION_NAME = os.environ['MEF_APPLICATION_NAME']
+MEF_PROJECT_NAME = os.environ['MEF_PROJECT_NAME']
 
 HOST_NAME = 'localhost'
-PORT_NUMBER = 8007
+PORT_NUMBER = 8080
+
+HOST_OS = MEF_APPLICATION_NAME + '.' + MEF_PROJECT_NAME + '.apps.test-ose.ca.sbrf.ru'
 
 
 class MyHandler(BaseHTTPRequestHandler):
@@ -15,27 +21,29 @@ class MyHandler(BaseHTTPRequestHandler):
         s.send_header("Content-type", "text/html")
         s.end_headers()
     def do_GET(s):
+        # print(res['result'])
+        # res = es.get(index="dictionary", doc_type='dictionary_type', id=1)
+        # print(res['_source'])
+        cloudDataLogging.logMetrics("test")
         s.send_response(200)
         s.send_header("Content-type", "text/html")
         s.end_headers()
-
-        print (time.asctime(), "FileBeat: Server Starts - %s:%s" % (HOST_NAME, PORT_NUMBER))
-        print (os.environ)
-        print (os.environ['OPENSHIFT_MEF_LOG_PATH'])
+        #
+        # print (time.asctime(), "FileBeat: Server Starts - %s:%s" % (HOST_NAME, PORT_NUMBER))
+        # print (os.environ)
         # if os.environ.get('OPENSHIFT_MEF_LOG_PATH') is not None:
         #     logging.basicConfig(level=logging.INFO)
         #     logging.info("NOOO", time.asctime())
         # else:
-        logging.basicConfig(filename='/var/lib/docker/containersSe/model.log', filemode='a', level=logging.INFO)
-        logging.info("YEES", time.asctime())
+        # logging.basicConfig(filename=os.environ['OPENSHIFT_MEF_LOG_PATH'] + '/model.log', filemode='a', level=logging.INFO)
 
-        logging.debug("GET DEBUG REQUEST time - %s", time.asctime())
-        logging.info("GET INFO REQUEST time - %s", time.asctime())
-        logging.warning("GET WARNING REQUEST time - %s", time.asctime())
+        # logging.debug("GET DEBUG REQUEST time - %s", time.asctime())
+        # logging.info("GET INFO REQUEST time - %s", time.asctime())
+        # logging.warning("GET WARNING REQUEST time - %s", time.asctime())
 
 if __name__ == '__main__':
     server_class = HTTPServer
-    httpd = server_class((HOST_NAME, PORT_NUMBER), MyHandler)
+    httpd = server_class((HOST_OS, PORT_NUMBER), MyHandler)
     print (time.asctime(), "Server Starts - %s:%s" % (HOST_NAME, PORT_NUMBER))
     try:
         httpd.serve_forever()
---
+++ cloudDataLogging.py
@@ -0,0 +1,25 @@
+from elasticsearch import Elasticsearch
+import json
+import uuid
+import socket
+
+ES_METRICS_TYPE = 'mef_model_metrics_type'
+ES_INDEX_PREFIX = 'spr-mef-'
+ES_MODEL_NAME = os.environ['MEF_APPLICATION_NAME']
+ES_HOST = os.environ['MEF_ES_HOSTS']
+ES_PORT = os.environ['MEF_ES_PORT']
+
+
+def logMetrics(metrics):
+    print (ES_MODEL_NAME)
+    print (ES_HOST)
+    print (ES_PORT)
+    data = {}
+    data['model_name'] = ES_MODEL_NAME
+    data['host_name'] = socket.gethostname()
+    data['metrics'] = metrics
+    json_metrics = json.dumps(data)
+    documentId = str(uuid.uuid4())
+    print ('documentId = ' + documentId)
+    es = Elasticsearch([{'host': ES_HOST, 'port': ES_PORT}])
+    res = es.index(index=ES_INDEX_PREFIX + ES_MODEL_NAME, doc_type=ES_METRICS_TYPE, id=documentId, body=json_metrics)
